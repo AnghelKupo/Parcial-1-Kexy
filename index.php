70e0180aed64fdb3d00e012254a34e76d3b9cb9c
Alik <moisesalik01@gmail.com>
2023-11-02 20:44:35 -0500

    cambios

--- index.php
+++ index.php
@@ -3,7 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+
+    <script src="./Public/js/tailwind.js"></script>
+
+    <title>Index</title>
 </head>
 <body>
     
