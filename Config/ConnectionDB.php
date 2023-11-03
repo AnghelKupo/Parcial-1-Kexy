14c889339be15da68e926f2b3626225ec41c0336
Ana Victoria <113152839+AnghelKupo@users.noreply.github.com>
2023-11-02 20:23:53 -0500

    Base de datos

--- Config/ConnectionDB.php
+++ Config/ConnectionDB.php
@@ -48,18 +48,4 @@ class ConnectionDB {
         $this->conn = null;
     }
 }
-
-
-$connection = ConnectionDB::getInstance()->getConnection();
-$sql = "SELECT * FROM users";
-
-try {
-    $resultado = $connection->query($sql);
-    
-    if($resultado->rowCount() > 0) {
-        $resultados = $resultado->fetchAll(PDO::FETCH_ASSOC);
-    }
-    echo"Lo he hecho";
-} catch (PDOException $exception) {
-    echo "error";
-}
+?>
